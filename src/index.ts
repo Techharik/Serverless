8a83a45c08bf10096c28c7e7a26fbfab0eea9f78
Hari Prasath <khariprasath30@gmail.com>
2024-11-29 07:36:19 +0530

    created a api reques using neative cloudeflare also with node native

--- src/index.ts
+++ src/index.ts
@@ -18,9 +18,53 @@
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return Response.json({
-			message: 'You get the response'
-		})
+		const { url, method } = request
+		const setUrl = url.replace('http://127.0.0.1:8787/', '') // /api/v1/users
 
-	},
+		if (setUrl == 'api/v1/users') {
+			if (method === 'GET') {
+				//connect with db - or from the controller file logic
+				return Response.json({
+					message: 'You get the response users'
+				})
+
+			} else if (method == 'POST') {
+				//-- db logic for post
+				return Response.json({
+					message: 'Data Added Successfully users'
+				})
+			} else {
+				return Response.json({
+					message: '404not found'
+				})
+			}
+
+		} else if (setUrl === 'api/v1/posts') {
+
+			if (method === 'GET') {
+				//connect with db - or from the controller file logic
+				return Response.json({
+					message: 'You get the response posts'
+				})
+
+			} else if (method == 'POST') {
+				//-- db logic for post
+				return Response.json({
+					message: 'Data Added Successfully posts'
+				})
+			}
+			else {
+				return Response.json({
+					message: 'Invalid routes'
+				})
+			}
+		} else {
+			return Response.json({
+				message: 'Invalid routes'
+			})
+		}
+
+
+	}
+
 } satisfies ExportedHandler<Env>;
